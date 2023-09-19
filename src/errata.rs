c1902865a7bfec6b95adfa69b3cbfaa58047ee3b
Per Lindgren <per.lindgren@ltu.se>
2023-09-19 08:11:02 +0200

    errata updates

--- src/errata.rs
+++ src/errata.rs
@@ -9,7 +9,7 @@ unsafe fn peek(addr: u32) -> u32 {
 }
 
 pub fn pre_enable() {
-    // Works around Erratum 187 on chip revisions 1 and 2.
+    // Works around Erratum 187 on chip revisions 1, 2 and 3.
     unsafe {
         poke(0x4006EC00, 0x00009375);
         poke(0x4006ED14, 0x00000003);
@@ -22,16 +22,24 @@ pub fn pre_enable() {
 pub fn post_enable() {
     post_wakeup();
 
-    // Works around Erratum 187 on chip revisions 1 and 2.
+    // Works around Erratum 187 on chip revisions 1, 2 and 3.
     unsafe {
         poke(0x4006EC00, 0x00009375);
         poke(0x4006ED14, 0x00000000);
         poke(0x4006EC00, 0x00009375);
     }
+
+    // Works around Erratum 166 on chip revisions 1, 2 and 3.
+    // fixes problem with double buffering
+    const NRF_USBD_BASE: u32 = 0x40027000;
+    unsafe {
+        poke(NRF_USBD_BASE + 0x800, 0x7E3);
+        poke(NRF_USBD_BASE + 0x804, 0x40);
+    }
 }
 
 pub fn pre_wakeup() {
-    // Works around Erratum 171 on chip revisions 1 and 2.
+    // Works around Erratum 171 on chip revisions 1, 2 and 3.
 
     unsafe {
         if peek(0x4006EC00) == 0x00000000 {
@@ -44,7 +52,7 @@ pub fn pre_wakeup() {
 }
 
 pub fn post_wakeup() {
-    // Works around Erratum 171 on chip revisions 1 and 2.
+    // Works around Erratum 171 on chip revisions 1, 2 and 3.
 
     unsafe {
         if peek(0x4006EC00) == 0x00000000 {
@@ -54,4 +62,15 @@ pub fn post_wakeup() {
         poke(0x4006EC14, 0x00000000);
         poke(0x4006EC00, 0x00009375);
     }
+}
+
+pub fn dma_start() {
+    // Works around Erratum 199 on chip revisions 1, 2 and 3.
+
+    unsafe { poke(0x40027C1C, 0x00000082) }
+}
+
+pub fn dma_complete() {
+    // Works around Erratum 199 on chip revisions 1, 2 and 3.
+    unsafe { poke(0x40027C1C, 0x00000000) }
 }
